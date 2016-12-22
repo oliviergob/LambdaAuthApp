b18e5ef6a3ad3d4ec10c26fbd42916ca54b60589
Gobs <olivier.gob@gmail.com>
2016-12-22 00:47:16 +0000

    Now a Single Page App + removed API Gateway stage for now

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,23 @@
 AWS.config.region = 'us-east-1'; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1:ec5d9472-1c1d-4fc0-a034-bb28669a654f',
+    IdentityPoolId: 'us-east-1:80561d71-faa2-46b1-9cec-dc03226b7f12',
     Logins: {
-      'cognito-idp.us-east-1.amazonaws.com/us-east-1_7UTEPaGZZ': JSON.parse(localStorage.getItem('token'))
+      'cognito-idp.us-east-1.amazonaws.com/us-east-1_j18XflhDT': JSON.parse(localStorage.getItem('token'))
     }
+});
+
+var lambda = new AWS.Lambda();
+
+
+$(".nav li").on("click", function() {
+    $(".nav li").removeClass("active");
+    $(this).addClass("active");
 });
 
 $(document).ready(function(){
   updateAuthenticationStatus();
   loadBasicData();
+
 });
 function logout(){
   localStorage.clear();
@@ -20,50 +29,73 @@ function updateAuthenticationStatus(){
   $('#login').empty();
   var user = localStorage.getItem('token');
   if(user){
-    $('#user').show().append('<a onclick="logout()">Log out</a>');
+    $('#user').show().append('<a href="#" onclick="logout()">Log out</a>');
     $('#login').hide();
   } else {
-    $('#login').show().append('<a href="/login.html">Log in</a>');
+    $('#login').show().append('<a href="#"onclick="login()">Log in</a>');
     $('#user').hide();
   }
 }
 
 
 function loadBasicData(){
-  if(localStorage.getItem('token')){
-    AWS.config.credentials.get(function (err) {
-      var client = apigClientFactory.newClient({
-        accessKey: AWS.config.credentials.accessKeyId,
-        secretKey: AWS.config.credentials.secretAccessKey,
-        sessionToken: AWS.config.credentials.sessionToken,
-        region: 'us-east-1'
-      });
-      client.basicdataaccessPost({}, {key:"Tralala"}, {})
-      .then(function(data){
-        if(data.data.httpStatus == 200){
-          $('#basicData').append('<div class="alert alert-success">'+ data.data.value +'</div>')
-        } else {
-          $('#basicData').append('<div class="alert alert-danger">'+ data.data.message +'</div>')
-        }
-      }).catch( function(result){
-        $('#basicData').append('<div class="alert alert-danger">Error retreiving data</div>')
-      })
-    });
+  $('#sensitiveDataContainer').hide();
+  $('#loginContainer').hide();
+  $('#basicDataContainer').show();
+
+
+
+  var myNode = document.getElementById("basicData");
+  while (myNode.firstChild) {
+      myNode.removeChild(myNode.firstChild);
   }
+
+  if(localStorage.getItem('token')){
+
+    var event={key:"randomKey"};
+    lambda.invoke({
+        FunctionName: 'basicDataAccess-development',
+        Payload: JSON.stringify(event, null, 2) // pass params
+      }, function(error, data) {
+        if (error) {
+          console.dir(error);
+          $('#basicData').append('<div class="alert alert-danger">Error retreiving data</div>')
+        }
+        else {
+          data=JSON.parse(data.Payload);
+          if(data.httpStatus == 200){
+            $('#basicData').append('<div class="alert alert-success">'+ data.value +'</div>')
+          } else {
+            $('#basicData').append('<div class="alert alert-danger">'+ data.message +'</div>')
+          }
+        }
+      });
+  }
+}
+
+
+function loadSensitiveData(){
+  $('#basicDataContainer').hide();
+  $('#loginContainer').hide();
+  $('#sensitiveDataContainer').show();
+}
+
+
+function login(){
+  $('#basicDataContainer').hide();
+  $('#loginContainer').show();
+  $('#sensitiveDataContainer').hide();
 }
 
 $('#signin').submit(function(e){
   e.preventDefault();
   AWSCognito.config.region = 'us-east-1';
-  AWSCognito.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1_7UTEPaGZZ'
-  });
   // Need to provide placeholder keys unless unauthorised user access is enabled for user pool
   AWSCognito.config.update({accessKeyId: 'anything', secretAccessKey: 'anything'});
 
   var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool({
-    UserPoolId : 'us-east-1_7UTEPaGZZ',
-    ClientId : '15lpt5oc409qopbtd2k1bjgfna'
+    UserPoolId : 'us-east-1_j18XflhDT',
+    ClientId : '7lhd9srvjsdfmnc23oribgjcir'
   });
 
   var authenticationData = {
@@ -83,7 +115,7 @@ $('#signin').submit(function(e){
       window.location = '/';
     },
     onFailure: function(err) {
-      console.log(err);
+      console.log("error authenticating user"+err);
     }
   });
 })
