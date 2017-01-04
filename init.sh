6d885930939c40e488ff2eec043ffd6f839d50c7
Gobs <olivier.gob@gmail.com>
2017-01-04 15:19:03 +0000

    Making some progress with deployment script and cloudformation stack

--- init.sh
+++ init.sh
@@ -24,7 +24,7 @@ region=$(jq -r '.region' config.json)
 appName=$(jq -r '.appName' config.json)
 appNameLowerCase=$(echo "$appName" | tr '[:upper:]' '[:lower:]')
 bucketName=$appNameLowerCase.$(jq -r '.bucket' config.json)
-ddbTable=$appName$(jq -r '.ddbTable' config.json)
+awsAccountNumber=$(jq -r '.awsAccountNumber' config.json)
 
 
 # Verifying the bucket name is Valid
@@ -42,16 +42,29 @@ fi
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 scriptDir="file://${scriptDir//////}//cloudformation//simpleApp.json"
 
+echo Creating Cognito User pool
+userPoolId=`aws cognito-idp create-user-pool --pool-name ${appName}UserPool --auto-verified-attributes email --admin-create-user-config AllowAdminCreateUserOnly=true --schema  '[{"Name":"name","Required":true}, {"Name":"email","Required":true}]' | jq -r '.UserPool.Id'`
+userPoolArn="arn:aws:cognito-idp:$region:$awsAccountNumber:userpool/$userPoolId"
+userPoolClientId=$(aws cognito-idp create-user-pool-client --user-pool-id $userPoolId --client-name ${appName} | jq -r '.UserPoolClient.ClientId')
+echo "Created User Pool ${appName}UserPool with ARN $userPoolArn and client ID $userPoolClientId"
+
+echo
+echo Creating Cognito Identity Pool
+identityPoolId=`aws cognito-identity create-identity-pool --identity-pool-name ${appName}IdPool --no-allow-unauthenticated-identities --cognito-identity-providers ProviderName=cognito-idp.$region.amazonaws.com/$userPoolId,ClientId=$userPoolClientId | jq -r '.IdentityPoolId'`
+echo "Created Identity Pool Id ${appName}IdPool with ID $identityPoolId"
 
 # Create S3 Bucket
+echo
 echo Creating cloudformation stack $appName in $region to create:
 echo "   - S3 bucket $bucketName"
-echo "   - DynamoDB table $ddbTable"
+#TODO Add role names as stack parameters
 aws cloudformation create-stack \
+    --capabilities CAPABILITY_NAMED_IAM \
     --stack-name $appName \
     --template-body $scriptDir \
     --parameters ParameterKey=BucketNameParam,ParameterValue=$bucketName \
-                 ParameterKey=DDBTableName,ParameterValue=$ddbTable \
+                 ParameterKey=CognidoIdentityPoolIdParam,ParameterValue=$identityPoolId \
+                 ParameterKey=CognidoUserPoolArnParam,ParameterValue=$userPoolArn \
     --region $region
 #TODO handle errors
 
@@ -63,6 +76,12 @@ aws cloudformation wait stack-create-complete \
     --region $region
 
 #TODO handle errors
+
+echo
+echo Associating Identity Pool with IAM Roles
+# TODO - get the roles from cloudformation outputs
+aws cognito-identity set-identity-pool-roles --identity-pool-id $identityPoolId --roles authenticated=arn:aws:iam::546190104433:role/SimpleAuthAppBasicUserRole --role-mappings "{\"cognito-idp.$region.amazonaws.com/$userPoolId:$userPoolClientId\": {\"Type\" : \"Token\", \"AmbiguousRoleResolution\": \"AuthenticatedRole\" } }"
+exit
 
 echo "Stack now fully created"
 
